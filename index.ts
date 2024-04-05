35922b514f85bff3dcbb61990db4350ac9287a25
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-04-05 23:41:01 +0500

    changes in file

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let todos = [];
+let condition = true;
+
+while(condition){
+    let addTask = await inquirer.prompt(
+        [
+            {
+                name: "todo",
+                message: "What Do You Want To Add In Todos?",
+                type: "input"
+            },
+            {
+                name: "addMore",
+                message: "Do You Want To Add More?",
+                type: "confirm",
+                default: "true"
+            }
+        ]
+    )
+    
+    todos.push(addTask.todo);
+    condition = addTask.addMore
+    console.log(todos);
+}
